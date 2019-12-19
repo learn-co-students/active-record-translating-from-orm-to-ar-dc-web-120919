fb2ad832ebb70e74d768374ace383db2cd469dec
Sean Beach <sean.beach1@yahoo.com>
2019-12-19 14:13:21 -0500

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -16,3 +16,13 @@ DB = ActiveRecord::Base.connection
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  breed TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+ 
